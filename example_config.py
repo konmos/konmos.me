290a72ccd5682031882a2ac7a733f8e3a4487ff1
konmos <32582880+konmos@users.noreply.github.com>
2019-10-09 15:28:11 +0100

    Create example_config.py

---
+++ example_config.py
@@ -0,0 +1,17 @@
+# TODO: This should be updated and renamed to `config.py` to run the project
+
+# mongodb
+DATABASE_HOST = 'localhost'
+DATABASE_PORT = 27017
+DATABASE_USER = None
+DATABASE_PASS = None
+
+# flask
+DEBUG = True
+SECRET_KEY = b'FILL_ME_IN'
+
+# b64decode('R0lGODlhAQABAIAAANvf7wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==')
+ANALYTICS_GIF = b'GIF89a\x01\x00\x01\x00\x80\x00\x00\xdb\xdf\xef\x00\x00\x00!\xf9\x04\x01' \
+                b'\x00\x00\x00\x00,\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02D\x01\x00;'
+
+EMAIL = 'FILL_ME_IN'
